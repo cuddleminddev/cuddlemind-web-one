3d59e9cd161ef461310a193d7a3d7265e051b93e
Kuriakose George <kuriakose00@gmail.com>
2025-06-09 11:40:47 +0530

    header fixes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  currentRoute:string = '';
 
+  constructor(private router: Router) {
+        this.currentRoute = this.router.url;
+
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+   }
+
+  navigateTo(path: string): void {
+    this.router.navigate([path]);
+  }
 }
